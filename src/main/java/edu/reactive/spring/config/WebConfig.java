14d04546e7e4bdad4331885685c86a3119bf77df
glenn <glenn.jo@kakaocorp.com>
2018-04-27 17:59:10 +0900

    add async

--- src/main/java/edu/reactive/spring/config/WebConfig.java
+++ src/main/java/edu/reactive/spring/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 
 //stereotype(Component, Controller, Service, Repository) 어노테이션이 붙은 클래스들을 스프링 컨테이너에 등록
 @ComponentScan("edu.reactive.spring")
-@EnableWebFlux
 @Configuration
 public class WebConfig {
 }
